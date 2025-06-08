babe14d4c65747d570061f5c668aaf69ccfb08a2
Warley Dias <72942099+wwddnn@users.noreply.github.com>
2025-06-08 09:14:49 -0300

    Criando o component Header

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header from "./components/Header";
+
+export default function App() {
+  return (
+    <>
+    <Header />
+
+    </>
+
+  )
+}
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+
+import './styles.css';
+
+export default function Header() {
+
+    return(
+        <header>
+            <div className="header-container">
+                <h1>DSCarrosTop</h1>
+            </div>
+        </header>
+    );
+}
