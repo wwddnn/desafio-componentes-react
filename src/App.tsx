d304c577b49108a83b61ae80728b5924c3d4467d
Warley Dias <72942099+wwddnn@users.noreply.github.com>
2025-06-13 15:59:38 -0300

    Criando componente Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import "./App.css";
+import Header from "./components/Header";
 import Card from "./components/Card";
 import Comments from "./components/Comments";
-import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <>
-    <Header />
+      <Header />
 
-    <main className="ct-main">
-      <section id="ct-card-section" className="ct-card-container">
-        <Card />
-      </section>
+      <main className="ct-main">
+        <section id="ct-card-section" className="ct-card-container">
+          <Card />
+        </section>
 
-      <section id="ct-comments-section" className="ct-comments-container">
-        <Comments />
-      </section>
-    </main>
+        <section id="ct-comments-section" className="ct-comments-container">
+          <Comments />
+        </section>
 
+        <section>
+          <Footer />
+        </section>
+      </main>
     </>
-
-  )
+  );
 }
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="card-footer">
+        <a href="#">@dscarrostop</a>
+        <address>Rua da Pipoca, 150</address>
+      </div>
+    </footer>
+  );
+}
